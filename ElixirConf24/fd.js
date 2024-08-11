80bd0cb96db6bf0c6c924a4fabf1bcc07fe556bf
Vanessa Lee <vanessaklee@gmail.com>
2024-08-11 14:49:30 -0500

    Update fd.js

--- ElixirConf24/fd.js
+++ ElixirConf24/fd.js
@@ -4,11 +4,11 @@ import {
 } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
 
 export async function init(ctx, html) {
-  ctx.importCSS("https://raw.githubusercontent.com/vanessaklee/Notebooks/main/ElixirConf24/fd.css");
+  ctx.importCSS("main.css");
   ctx.root.innerHTML = html;
 
   async function run() {
-    import { FaceDetector, FilesetResolver } from "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0";
+    console.log("Starting.....");
     const demosSection = document.getElementById("demos");
     let faceDetector;
     let runningMode = "IMAGE";
@@ -52,11 +52,6 @@ export async function init(ctx, html) {
         if (!faceDetector) {
             console.log("Wait for objectDetector to load before clicking");
             return;
-        }
-        // if video mode is initialized, set runningMode to image
-        if (runningMode === "VIDEO") {
-            runningMode = "IMAGE";
-            await faceDetector.setOptions({ runningMode: "IMAGE" });
         }
         const ratio = event.target.height / event.target.naturalHeight;
         // faceDetector.detect returns a promise which, when resolved, is an array of Detection faces
@@ -117,126 +112,7 @@ export async function init(ctx, html) {
             }
         }
     }
-    /********************************************************************
-     // Demo 2: Continuously grab image from webcam stream and detect it.
-     ********************************************************************/
-    let video = document.getElementById("webcam");
-    const liveView = document.getElementById("liveView");
-    let enableWebcamButton;
-    // Check if webcam access is supported.
-    const hasGetUserMedia = () => { var _a; return !!((_a = navigator.mediaDevices) === null || _a === void 0 ? void 0 : _a.getUserMedia); };
-    // Keep a reference of all the child elements we create
-    // so we can remove them easilly on each render.
-    var children = [];
-    // If webcam supported, add event listener to button for when user
-    // wants to activate it.
-    if (hasGetUserMedia()) {
-        enableWebcamButton = document.getElementById("webcamButton");
-        enableWebcamButton.addEventListener("click", enableCam);
-    }
-    else {
-        console.warn("getUserMedia() is not supported by your browser");
-    }
-    // Enable the live webcam view and start detection.
-    async function enableCam(event) {
-        if (!faceDetector) {
-            alert("Face Detector is still loading. Please try again..");
-            return;
-        }
-        // Hide the button.
-        enableWebcamButton.classList.add("removed");
-        // getUsermedia parameters
-        const constraints = {
-            video: true
-        };
-        // Activate the webcam stream.
-        navigator.mediaDevices
-            .getUserMedia(constraints)
-            .then(function (stream) {
-            video.srcObject = stream;
-            video.addEventListener("loadeddata", predictWebcam);
-        })
-            .catch((err) => {
-            console.error(err);
-        });
-    }
-    let lastVideoTime = -1;
-    async function predictWebcam() {
-        // if image mode is initialized, create a new classifier with video runningMode
-        if (runningMode === "IMAGE") {
-            runningMode = "VIDEO";
-            await faceDetector.setOptions({ runningMode: "VIDEO" });
-        }
-        let startTimeMs = performance.now();
-        // Detect faces using detectForVideo
-        if (video.currentTime !== lastVideoTime) {
-            lastVideoTime = video.currentTime;
-            const detections = faceDetector.detectForVideo(video, startTimeMs)
-                .detections;
-            displayVideoDetections(detections);
-        }
-        // Call this function again to keep predicting when the browser is ready
-        window.requestAnimationFrame(predictWebcam);
-    }
-    function displayVideoDetections(detections) {
-        // Remove any highlighting from previous frame.
-        for (let child of children) {
-            liveView.removeChild(child);
-        }
-        children.splice(0);
-        // Iterate through predictions and draw them to the live view
-        for (let detection of detections) {
-            const p = document.createElement("p");
-            p.innerText =
-                "Confidence: " +
-                    Math.round(parseFloat(detection.categories[0].score) * 100) +
-                    "% .";
-            p.style =
-                "left: " +
-                    (video.offsetWidth -
-                        detection.boundingBox.width -
-                        detection.boundingBox.originX) +
-                    "px;" +
-                    "top: " +
-                    (detection.boundingBox.originY - 30) +
-                    "px; " +
-                    "width: " +
-                    (detection.boundingBox.width - 10) +
-                    "px;";
-            const highlighter = document.createElement("div");
-            highlighter.setAttribute("class", "highlighter");
-            highlighter.style =
-                "left: " +
-                    (video.offsetWidth -
-                        detection.boundingBox.width -
-                        detection.boundingBox.originX) +
-                    "px;" +
-                    "top: " +
-                    detection.boundingBox.originY +
-                    "px;" +
-                    "width: " +
-                    (detection.boundingBox.width - 10) +
-                    "px;" +
-                    "height: " +
-                    detection.boundingBox.height +
-                    "px;";
-            liveView.appendChild(highlighter);
-            liveView.appendChild(p);
-            // Store drawn objects in memory so they are queued to delete at next call
-            children.push(highlighter);
-            children.push(p);
-            for (let keypoint of detection.keypoints) {
-                const keypointEl = document.createElement("spam");
-                keypointEl.className = "key-point";
-                keypointEl.style.top = `${keypoint.y * video.offsetHeight - 3}px`;
-                keypointEl.style.left = `${video.offsetWidth - keypoint.x * video.offsetWidth - 3}px`;
-                liveView.appendChild(keypointEl);
-                children.push(keypointEl);
-            }
-        }
-    }
-
-
   }
+
   run();
 }
